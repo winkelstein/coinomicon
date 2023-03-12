0ec722d24b76105e73ece9829d3f583eab63070d
treug0lnik041 <treug0lnik@yahoo.com>
2023-03-13 02:21:51 +0300

    fix: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,16 @@ async function main() {
 
     await coinomicon.deployed();
 
-    console.log(`Coinomicon deployed to ${coinomicon.address}`);
+    const CoinomiconExchangeImpl = await ethers.getContractFactory("CoinomiconExchangeImpl");
+    const coinomiconExchangeImpl = await CoinomiconExchangeImpl.deploy();
+
+    await coinomiconExchangeImpl.deployed();
+
+    await coinomicon._setExchangeImplementation(coinomiconExchangeImpl.address);
+
+    console.log(
+        `Coinomicon deployed to ${coinomicon.address}; Exchange implementation deployed to ${coinomiconExchangeImpl.address}`
+    );
 }
 
 main().catch((error) => {
