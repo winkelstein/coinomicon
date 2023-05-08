e5f7e936dad764b29f749a1ba279fbdbb18ded1f
winkelstein <andrew.winkelstein@gmail.com>
2023-05-09 02:03:56 +0300

    feat(contract): npm -> yarn

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,42 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@openzeppelin/hardhat-upgrades";
 require("dotenv").config();
 
-const config: HardhatUserConfig = {
-    solidity: {
-        version: "0.8.17",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 200
-            }
-        }
-    },
-    networks: {
-        goerli: {
-            url: "https://goerli.infura.io/v3/6874a7fbb0ac4f7c96881e168369ee49",
-            accounts: [process.env.GOERLI_PRIVATE_KEY as string]
-        }
-    },
-    etherscan: {
-        apiKey: {
-            goerli: process.env.GOERLISCAN_API_KEY as string
-        }
-    }
-};
+const config: HardhatUserConfig = process.env.GOERLI_PRIVATE_KEY
+    ? {
+          solidity: {
+              version: "0.8.17",
+              settings: {
+                  optimizer: {
+                      enabled: true,
+                      runs: 200
+                  }
+              }
+          },
+          defaultNetwork: "hardhat",
+          networks: {
+              hardhat: {},
+              goerli: {
+                  url: "https://goerli.infura.io/v3/6874a7fbb0ac4f7c96881e168369ee49",
+                  accounts: [process.env.GOERLI_PRIVATE_KEY as string]
+              }
+          },
+          etherscan: {
+              apiKey: {
+                  goerli: process.env.GOERLISCAN_API_KEY as string
+              }
+          }
+      }
+    : {
+          solidity: {
+              version: "0.8.17",
+              settings: {
+                  optimizer: {
+                      enabled: true,
+                      runs: 200
+                  }
+              }
+          }
+      };
 
 export default config;
